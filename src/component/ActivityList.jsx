f01e5ff2a243457859498f33faab775c261fe571
Lena Imdahl <mail@lenaimdahl.de>
2023-09-07 08:25:28 +0200

    deleted unused component, added activity list

--- src/component/ActivityList.jsx
+++ src/component/ActivityList.jsx
@@ -1,17 +1,21 @@
-import ActivityItem from "./ActivityItem";
-
 function ActivityList({ fetchActivities, activities }) {
   return (
     <div className="vertical-list">
       <h3>Activities</h3>
-      <ul>
-        <ActivityItem color={"pink"} type={"Work"} />
-        <ActivityItem color={"yellow"} type={"Kids"} />
-        <ActivityItem color={"green"} type={"Household"} />
-        <ActivityItem color={"blue"} type={"Sleep"} />
-        <ActivityItem color={"red"} type={"Eat"} />
-        <ActivityItem color={"orange"} type={"Workout"} />
-      </ul>
+      {activities.map((activity, index) => (
+        <table key={index}>
+          <tr>
+            <td
+              style={{
+                backgroundColor: activity.color,
+                width: 15,
+                height: 15,
+              }}
+            ></td>
+            <td>{activity.type}</td>
+          </tr>
+        </table>
+      ))}
     </div>
   );
 }
