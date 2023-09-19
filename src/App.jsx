19c5380dd8dad1405fecdd9e9cd6211ac1b7e217
Lena Imdahl <mail@lenaimdahl.de>
2023-09-19 18:49:08 +0200

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 import "./css/index.css";
+import Home from "./pages/Home";
+import Overview from "./pages/Overview";
+import Statistics from "./pages/Statistics";
+import History from "./pages/History";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,16 @@ import "./css/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GlobalContextWrapper } from "./context/global.context";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalContextWrapper>
-      <App />
+      <Router>
+        {" "}
+        <App />
+      </Router>
     </GlobalContextWrapper>
   </React.StrictMode>
 );
