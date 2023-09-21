02cf6c7a9d20b1665433d9f01f2f603971c2b4ee
Lena Imdahl <mail@lenaimdahl.de>
2023-09-21 08:21:32 +0200

    added clickable box to activity card

--- src/component/ActivityList.jsx
+++ src/component/ActivityList.jsx
@@ -1,15 +1,23 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context/global.context";
 
 function ActivityList({ activities }) {
   const { backendAPIInstance } = useContext(GlobalContext);
+  const [clickedActivityId, setClickedActivityId] = useState(null);
 
   const handleFinishedActivity = async (id, event) => {
     event.preventDefault();
-    try {
-      await backendAPIInstance.saveFinishedActivity(id);
-    } catch (error) {
-      console.error(error);
+    if (clickedActivityId === id) {
+      setClickedActivityId(null);
+    } else {
+      setClickedActivityId(id);
+      try {
+        await backendAPIInstance.saveFinishedActivity(id);
+        //setIsClicked(false)
+        //backgroundbolor of bos back to grey
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -18,32 +26,30 @@ function ActivityList({ activities }) {
       <h3>All Activities</h3>
       <div className="flex-wrap">
         {activities.map((activity) => (
-          <div className="card">
-            <div>
-              <div
-                className="box"
-                onClick={(event) => handleFinishedActivity(activity._id, event)}
-              ></div>
-              <div key={activity._id}>
-                <div
-                  style={{
-                    color: "red",
-                    fontSize: 18,
-                    fontWeight: "bold",
-                  }}
-                >
-                  {activity.points} 🏆
-                </div>
-              </div>
-              <div
-                style={{
-                  paddingTop: 10,
-                  fontSize: 18,
-                  fontWeight: "bold",
-                }}
-              >
-                {activity.name}
-              </div>
+          <div className="card" key={activity._id}>
+            <div
+              className={`box ${
+                clickedActivityId === activity._id ? "green-box" : ""
+              }`}
+              onClick={(event) => handleFinishedActivity(activity._id, event)}
+            ></div>
+            <div
+              style={{
+                color: "red",
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              {activity.points} 🏆
+            </div>
+            <div
+              style={{
+                paddingTop: 10,
+                fontSize: 18,
+                fontWeight: "bold",
+              }}
+            >
+              {activity.name}
             </div>
           </div>
         ))}
